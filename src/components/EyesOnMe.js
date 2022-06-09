0320ff66b8b113da06d374d2f6177d83c485df6b
freddex <fredjuiceman@yahoo.com>
2022-06-08 23:08:54 -0400

    commit tree ca50e4fb27e07f893ac58e3bb9628748e000bfd4

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,24 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+function EyesOnMe() {
+
+    function photoFocus() {
+        console.log('Good!')             
+    }
+
+    function photoBlur() {
+        console.log('Hey! Eyes on me!')        
+    }
+
+    return (
+        <button 
+            onFocus={photoFocus} onBlur={photoBlur}>Eyes on me</button> 
+    )
+} 
+
+export default EyesOnMe 
+
+
+
+
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
 // Code Keypad Component Here
+import React from 'react'
+
+function Keypad() {
+
+    function newKeypad() {
+        console.log('Entering password...')
+    }
+
+    return (
+        <input type='password' onChange={newKeypad}/>
+    )
+}
+
+export default Keypad
